5468ca8accbaacefe0b475ee4ee58c6cc801fdce
Tataru Cristi <tataru.cristi5@gmail.com>
2021-10-16 20:45:31 +0300

    added the logic

--- script.js
+++ script.js
@@ -43,8 +43,43 @@ const gameBoard = (() => {
         else return playerO.getSign();
     }
 
-    const increaseRound = () => {
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, getPlayerSign());
+        if(checkWinner(fieldIndex)) {
+            displayController.setResultMessage(getPlayerSign())
+            isOver = true;
+            round = 1;
+            return;
+        }
+        if(round === 9) {
+            displayController.setResultMessage("Draw");
+            isOver = true;
+            round = 1;
+            return;
+        }
         round++;
+        displayController.updateMessage();
+    }
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+
+          return winConditions
+      .filter((combination) => combination.includes(fieldIndex))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameBoard.getField(index) === getPlayerSign()
+        )
+      );
     }
 
     const reset = () => {
@@ -52,7 +87,7 @@ const gameBoard = (() => {
         isOver = false;
     }
 
-    return { getIsOver, getPlayerSign, increaseRound, reset };
+    return { getIsOver, getPlayerSign, reset, playRound };
 })();
 
 
@@ -66,9 +101,8 @@ const displayController = (() => {
         field.addEventListener('click', (e) => {
             if(gameController.getIsOver() || e.target.textContent !== '') return;
             gameBoard.setField(parseInt(e.target.dataset.index), gameController.getPlayerSign());
+            gameController.playRound(parseInt(e.target.dataset.index));
             updateGameboard();
-            gameController.increaseRound();
-            updateMessage();
         })
     })
 
@@ -85,9 +119,19 @@ const displayController = (() => {
           }
     }
 
+    const setResultMessage = (winner) => {
+        if(winner === "Draw") {
+            messageElement.textContent = "It's a draw!"; 
+        } else {
+            messageElement.textContent = `Player ${winner} has won!`;
+        }
+    }
+
     const updateMessage = () => {
         messageElement.textContent = `Player ${gameController.getPlayerSign()} turn`;
     }
+
+    return { setResultMessage, updateMessage }
 })();
 
 
